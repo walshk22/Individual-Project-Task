d7a2f9358c082c459d91c4ee19b21b5af28a9b98
walshk22 <43147647+walshk22@users.noreply.github.com>
2018-11-14 09:45:51 +0000

    Add files via upload
    New main function that prints possible user
    Another file that retrieves and prints a list of repostories a user has, using my github repos for the example

---
+++ ListUserRepos.hs
@@ -0,0 +1,68 @@
+module ListUserRepos where
+
+import qualified Github.Repos as Github
+import Data.List
+import Data.Maybe
+
+main = do
+  possibleRepos <- Github.userRepos "walshk22" Github.Owner
+  case possibleRepos of
+       (Left error)  -> putStrLn $ "Error: " ++ (show error)
+       (Right repos) -> putStrLn $ intercalate "\n\n" $ map formatRepo repos
+
+formatRepo repo =
+  (Github.repoName repo) ++ "\t" ++
+    (fromMaybe "" $ Github.repoDescription repo) ++ "\n" ++
+    (Github.repoHtmlUrl repo) ++ "\n" ++
+    (fromMaybe "" $ Github.repoCloneUrl repo) ++ "\t" ++
+    (formatDate $ Github.repoUpdatedAt repo) ++ "\n" ++
+    formatLanguage (Github.repoLanguage repo) ++
+    "watchers: " ++ (show $ Github.repoWatchers repo) ++ "\t" ++
+    "forks: " ++ (show $ Github.repoForks repo)
+
+formatDate (Just date) = show . Github.fromDate $ date
+formatDate Nothing = ""
+
+formatLanguage (Just language) = "language: " ++ language ++ "\t"
+formatLanguage Nothing = ""{-# LANGUAGE OverloadedStrings #-}
+module UserInfo (main) where
+
+import Common
+import Prelude ()
+
+import Data.Maybe (fromMaybe)
+
+import qualified GitHub
+import qualified GitHub.Endpoints.Users as GitHub
+
+main :: IO ()
+main = do
+  auth <- getAuth
+  possibleUser <- GitHub.userInfoFor' auth "mike-burns"
+  putStrLn $ either (("Error: " <>) . tshow) formatUser possibleUser
+
+formatUser :: GitHub.User -> Text
+formatUser user =
+  (formatName userName login) <> "\t" <> (fromMaybe "" company) <> "\t" <>
+    (fromMaybe "" location) <> "\n" <>
+    (fromMaybe "" blog) <> "\t" <> "<" <> (fromMaybe "" email) <> ">" <> "\n" <>
+    GitHub.getUrl htmlUrl <> "\t" <> tshow createdAt <> "\n" <>
+    "hireable: " <> formatHireable (fromMaybe False isHireable) <> "\n\n" <>
+    (fromMaybe "" bio)
+  where
+    userName = GitHub.userName user
+    company = GitHub.userCompany user
+    location = GitHub.userLocation user
+    blog = GitHub.userBlog user
+    email = GitHub.userEmail user
+    htmlUrl = GitHub.userHtmlUrl user
+    createdAt = GitHub.userCreatedAt user
+    bio = GitHub.userBio user
+
+formatName :: Maybe Text -> GitHub.Name GitHub.User -> Text
+formatName Nothing login = GitHub.untagName login
+formatName (Just name) user = name <> "(" <> GitHub.untagName login <> ")"
+
+formatHireable :: Bool -> Text
+formatHireable True = "yes"
+formatHireable False = "no"
--- Main.hs
+++ Main.hs
@@ -1,26 +1,7 @@
-module Main where
 
-import Lib
+import qualified GitHub as GH
 
-main :: IO ()
-main = someFunc
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-
-import Prelude.Compat
-
-import Data.Text         (Text, pack)
-import Data.Text.IO as T (putStrLn)
-import Data.Monoid       ((<>))
-
-import qualified GitHub.Endpoints.Users.Followers as GitHub
-
-main :: IO ()
+main :: IO()
 main = do
-    possibleUsers <- GitHub.usersFollowing "mike-burns"
-    T.putStrLn $ either (("Error: " <>) . pack . show)
-                        (foldMap ((<> "\n") . formatUser))
-                        possibleUsers
-
-formatUser :: GitHub.SimpleUser -> Text
-formatUser = GitHub.untagName . GitHub.simpleUserLogin
+     possibleUser <- GH.executeRequest' $ GH.userInfoR "walshk22"
+     print possibleUser
