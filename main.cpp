7dcb3b3cfad26653bf7eead88b5e24e18be24e06
walshk22 <43147647+walshk22@users.noreply.github.com>
2018-11-21 09:21:21 +0000

    Add files via upload
    back to c++--- testing with link given from tutorial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 //GATHERING DATA FROM GITHUB API AND POSTING INFO ON GITHUB
 
 
-#include <iostream>
+/*#include <iostream>
 
-//#include "restc-cpp/restc-cpp.h"
+#include "restc-cpp/restc-cpp.h"
 #include "restc-cpp.h"
 #include "restc-cpp/logging.h"
 
@@ -61,4 +61,35 @@ int main()
     .Data(data_object)                                 // Data object to send
 .   Execute();
     
+}*/
+
+#include <iostream>
+#include "restc-cpp/restc-cpp.h"
+
+using namespace std;
+using namespace restc_cpp;
+
+void DoSomethingInteresting(Context& ctx) {
+    // Here we are in a co-routine, running in a worker-thread.
+    
+    // Asynchronously connect to a server and fetch some data.
+    //FOR TESTING
+    auto reply = ctx.Get("http://jsonplaceholder.typicode.com/posts/1");
+    
+    //auto reply = ctx.Get("https://api.github.com/user");
+    // Asynchronously fetch the entire data-set and return it as a string.
+    auto json = reply->GetBodyAsString();
+    
+    // Just dump the data.
+    cout << "Received data: " << json << endl;
+}
+
+int main() {
+    auto rest_client = RestClient::Create();
+    
+    // Call DoSomethingInteresting as a co-routine in a worker-thread.
+    rest_client->Process(DoSomethingInteresting);
+    
+    // Wait for the coroutine to finish, then close the client.
+    rest_client->CloseWhenReady(true);
 }
